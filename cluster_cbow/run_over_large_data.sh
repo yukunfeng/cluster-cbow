b4f92fd429f39b9166fbb1bd4d40eba20e5516f5
yukunfeng <yukunfg@gmail.com>
2021-09-07 23:19:20 +0900

    minor

--- cluster_cbow/run_over_large_data.sh
+++ cluster_cbow/run_over_large_data.sh
@@ -4,12 +4,12 @@ set -x
 
 
 # Debug
-input=/home/lr/yukun/large_scale_corpus/sample.middle
-cluster="${input}.cluster.1600"
-output="${input}.fasttext.emb"
-gdb --args ./fasttext cbow -input $input -minCount 5 -dim 300 -thread 40 \
-    -output $output -epoch 10 -minn 3 -maxn 6 -neg 10 -loss ns \
-    -cluster $cluster -freq_thre_out 100 -lowersearch_cluster 
+# input=/home/lr/yukun/large_scale_corpus/sample.middle
+# cluster="${input}.cluster.1600"
+# output="${input}.fasttext.emb"
+# gdb --args ./fasttext cbow -input $input -minCount 5 -dim 300 -thread 40 \
+    # -output $output -epoch 10 -minn 3 -maxn 6 -neg 10 -loss ns \
+    # -cluster $cluster -freq_thre_out 100 -lowersearch_cluster 
 
 
 # subword-cbow without using word clusters.
@@ -20,9 +20,9 @@ gdb --args ./fasttext cbow -input $input -minCount 5 -dim 300 -thread 40 \
     # -output $output -epoch 10 -minn 3 -maxn 6 -neg 10 -loss ns
 
 # subword-cbow with word clusters
-# input=/home/lr/yukun/large_scale_corpus/preprocessed_wiki.txt.no_rare.txt.shuf
-# output="${input}.fasttext.outfreq100.emb"
-# cluster="${input}.cluster.1600"
-# ./fasttext cbow -input $input -minCount 5 -dim 300 -thread 40 \
-    # -output $output -epoch 10 -minn 3 -maxn 6 -neg 10 -loss ns \
-    # -cluster $cluster -freq_thre_out 100 -lowersearch_cluster 
+input=/home/lr/yukun/large_scale_corpus/preprocessed_wiki.txt.no_rare.txt.shuf
+output="${input}.fasttext.outfreq100.emb"
+cluster="${input}.cluster.1600"
+./fasttext cbow -input $input -minCount 5 -dim 300 -thread 40 \
+    -output $output -epoch 10 -minn 3 -maxn 6 -neg 10 -loss ns \
+    -cluster $cluster -freq_thre_out 100 -lowersearch_cluster 
